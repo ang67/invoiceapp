f762937eb60140add7a10c06e7e0788a475c6134
ang67 <binikouadiojosias@gmail.com>
2021-09-09 23:50:37 +0200

    version 1

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import { Table, Button } from 'reactstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsDown, faThumbsUp, faThumbs, faImage, faMoneyCheckAlt, faSearchDollar} from '@fortawesome/free-solid-svg-icons'
+class App extends Component {
+    state = {
+        isLoading: false,
+        invoices: [
+            {
+                "id": "100",
+                "Vendor": "Bini",
+                "Amount": "$12,000",
+                "Invoice": "1526",
+                "Date": "09/09/2021"
+            },
+            {
+                "id": "200",
+                "Vendor": "Bini",
+                "Amount": "$12,000",
+                "Invoice": "1526",
+                "Date": "09/09/2021"
+            },
+            {
+                "id": "400",
+                "Vendor": "Bini",
+                "Amount": "$12,000",
+                "Invoice": "1526",
+                "Date": "09/09/2021"
+            },
+            {
+                "id": "500",
+                "Vendor": "Bini",
+                "Amount": "$12,000",
+                "Invoice": "1526",
+                "Date": "09/09/2021"
+            },
+        ]
+    }
+    remove(id){
+        let updatedInvoices = [...this.state.invoices].filter (i => i.id !== id)
+        this.setState({invoices: updatedInvoices});
+    }
+    render() {
+        const isLoading = this.state.isLoading;
+        const allinvoices = this.state.invoices;
+
+        
+        if (isLoading)
+            return (<div>Loading...</div>)
+
+        let invoices = allinvoices.map( invoice => 
+            <tr key={invoice.id}>
+                <td>{invoice.Vendor}</td>
+                <td>{invoice.Amount}</td>
+                <td>{invoice.Invoice}</td>
+                <td>{invoice.Date}</td>
+                <td><Button className="btn btn-lg btn-success" onClick={() => this.remove(invoice.id)}> <FontAwesomeIcon icon={faThumbsUp}/>OK</Button></td>
+                <td><Button className="btn btn-lg btn-danger" onClick={() => this.remove(invoice.id)} ><FontAwesomeIcon icon={faThumbsDown}/>NOK</Button></td>
+                <td><Button className="btn btn-lg btn-info" onClick={() => this.remove(invoice.id)} ><FontAwesomeIcon icon={faMoneyCheckAlt}/>50%</Button></td>
+                <td><Button className="btn btn-lg btn-warning" onClick={() => this.remove(invoice.id)} ><FontAwesomeIcon icon={faSearchDollar}/>??</Button></td>
+                <td><Button className="btn btn-lg btn-info" onClick={() => this.remove(invoice.id)} ><FontAwesomeIcon icon={faImage}/>Image</Button></td>
+                
+                
+            </tr>
+
+        );
+        return (
+            <div className="container border border-secondary rouded center">
+                <div className="row">
+                    <div className="col-12">
+                        <h4>Pending Invoices - The test company</h4>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className=".col-xs-12 center text-center">
+                        <Table dark  responsive striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>vendor</th>
+                                    <th>Amount</th>
+                                    <th>Invoice #</th>
+                                    <th>Date</th>
+                                    <th colSpan="4">Actions</th>
+                                    <th>Image</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {this.state.invoices.length === 0 ? <td colSpan="9">All Caught up!</td> : invoices}
+                            </tbody>
+                        </Table>
+                    </div>
+                </div>
+                
+            </div>
+        );
+    }
+}
+
+
+export default App;
